17d8b499a7771cce7640a5843315b27c807dfa60
dibya <dibya@Dibyaranjan>
2025-10-25 23:23:08 +0530

    Tl commit

--- GitProj02/src/main/java/com/nt/payment/UPIPayment.java
+++ GitProj02/src/main/java/com/nt/payment/UPIPayment.java
@@ -4,6 +4,9 @@ public class UPIPayment {
 	
 	
 	public String doPaymenyUsingPhonePe(long phoneno,double amount) {
-		return amount+" Payment is happening from the phone no ::"+phoneno;
+		return amount+" Payment is happening from the phone no ::"+phoneno+" using Phonepe";
+	}
+	public String doPaymenyUsingGPay(long phoneno,double amount) {
+		return amount+" Payment is happening from the phone no ::"+phoneno+" using GPay";
 	}
 }
