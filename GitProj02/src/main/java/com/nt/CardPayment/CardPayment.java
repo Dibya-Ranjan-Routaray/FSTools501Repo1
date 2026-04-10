b7da3c460fbb99eaf0f1f3b30b2890455317ae3d
dibya <dibya2939@gmail.com>
2026-04-11 02:02:53 +0530

    nb

--- GitProj02/src/main/java/com/nt/CardPayment/CardPayment.java
+++ GitProj02/src/main/java/com/nt/CardPayment/CardPayment.java
@@ -2,6 +2,6 @@ package com.nt.CardPayment;
 
 public class CardPayment {
 	public String usingDebitCard(long cardno,double amount) {
-		return cardno+" debited using DebitCard "+amount;
+		return cardno+" debited using DebitCard "+amount;nbknjbn
 	}
 }
